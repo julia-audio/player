12600a31b0a8c39c6049c7b894e523599477ec46
joepasss@gentoo <farwes1180@gmail.com>
2025-03-22 10:13:35 +0900

    play song via miniaudio low api & print tag via taglib

--- src/main.c
+++ src/main.c
@@ -2,14 +2,26 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+#include <taglib/tag_c.h>
 #include "miniaudio.h"
-#include "wav_reader.h"
 
 void print_usage() {
 	printf("Usage: player [OPTIONS] <.wav file>\n");
 	printf("Options:\n");
 	printf("  -v, --verbose  show wav header information\n");
 	printf("  -h, --help     show this help message\n");
+}
+
+void data_callback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount) {
+	ma_decoder* pDecoder = (ma_decoder *)pDevice->pUserData;
+	if (pDecoder == NULL) {
+		return;
+	}
+
+	ma_decoder_read_pcm_frames(pDecoder, pOutput, frameCount, NULL);
+
+	(void)pInput;
 }
 
 int main(int argc, char *argv[]) {
@@ -42,37 +54,66 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 
-	if (check_file_format(file) != 0) {
-		fprintf(stderr, "failed file format check \n");
-		fclose(file);
-		return 1;
-	}
-
-	rewind(file);
-
-	struct wav_header wh;
-	wh.riff_header = read_riff_header(file);
-	wh.fmt_subchunk = read_fmt_subchunk(file);
-	wh.data_subchunk = read_data_subchunk(file);
-
-	if (verbose)
-		print_wav_header_info(wh);
-
 	// play sound
 	ma_result result;
-	ma_engine engine;
+	ma_decoder decoder;
+	ma_device_config deviceConfig;
+	ma_device device;
 
-	result = ma_engine_init(NULL, &engine);
+	result = ma_decoder_init_file(filename, NULL, &decoder);
 	if (result != MA_SUCCESS) {
-		return result;
+		return -2;
 	}
 
-	ma_engine_play_sound(&engine, filename, NULL);
+	deviceConfig = ma_device_config_init(ma_device_type_playback);
+	deviceConfig.playback.format = decoder.outputFormat;
+	deviceConfig.playback.channels = decoder.outputChannels;
+	deviceConfig.sampleRate = decoder.outputSampleRate;
+	deviceConfig.dataCallback = data_callback;
+	deviceConfig.pUserData = &decoder;
 
-	printf("Press Enter to quit");
+	if (ma_device_init(NULL, &deviceConfig, &device) != MA_SUCCESS) {
+		printf("Failed to open playback device.\n");
+		ma_decoder_uninit(&decoder);
+		return -3;
+	}
+
+	if (ma_device_start(&device) != MA_SUCCESS) {
+		printf("Failed to start playback device.\n");
+		
+		ma_device_uninit(&device);
+		ma_decoder_uninit(&decoder);
+		return -4;
+	}
+
+	// information print
+	TagLib_File *tag_file = taglib_file_new(filename);
+	const TagLib_Tag *tag = taglib_file_tag(tag_file);
+
+	if (tag == NULL) {
+		fprintf(stderr, "No tag found in file. \n");
+		taglib_file_free(tag_file);
+	}
+
+	if (verbose) {
+		printf("Title: %s\n", taglib_tag_title(tag));
+		printf("Artist: %s\n", taglib_tag_artist(tag));
+		printf("Album: %s\n", taglib_tag_album(tag));
+		printf("Year: %d\n", taglib_tag_year(tag));
+		printf("Track: %d\n", taglib_tag_track(tag));
+		printf("Genre: %s\n", taglib_tag_genre(tag));
+
+		printf("channels: %i\n", decoder.outputChannels);
+		printf("sample rate: %i\n", decoder.outputSampleRate);
+	}
+
+	printf("Press Enter to quit...");
 	getchar();
 
-	ma_engine_uninit(&engine);
+	// cleanup
+	ma_device_uninit(&device);
+	ma_decoder_uninit(&decoder);
+	taglib_file_free(tag_file);
 	
 	fclose(file);
 
